73dc103a35b87fd94079053c96dd9a52d6361966
Alexander Foremny <alexanderforemny@gmail.com>
2012-09-22 13:09:07 +0200

    Add features to rafextract
    Add the following command line flags.
    
      -b --base            Extract file to basename instead of full file
                           path.
      -C --directory=ITEM  Change into DIR before extraction.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-rafutils: Collection of utility programs to work with Riot File Archive (RAF) files.
+rafutils: Collection of utility programs to work with Riot File Archive (RAF)
+files.
+
+Tools
+-----
+
+* raflist: List file paths in a .RAF file.
+* rafextract: Extract file paths from a .RAF and .RAF.DAT file.
+
--- src/Extract.hs
+++ src/Extract.hs
@@ -3,12 +3,14 @@ module Main where
 
 import           Codec.Compression.Zlib
 import           Control.Applicative
+import           Control.Monad
 import           Data.ByteString.Lazy    ( ByteString )
 import           Data.Data
 import           Data.Maybe
 import           Data.RAF.Combined
 import           Data.Typeable
 import           System.Console.CmdArgs
+import           System.Directory
 import           System.Exit
 import           System.FilePath
 import           System.IO
@@ -17,16 +19,18 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.Map             as M
 
 data Extract = Extract
-    { rafFile :: FilePath
-    , datFile :: Maybe FilePath
-    , targets :: [FilePath]
+    { rafFile   :: FilePath
+    , datFile   :: Maybe FilePath
+    , targets   :: [FilePath]
+    , baseName  :: Bool
+    , changeDir :: Maybe FilePath
     } deriving (Show, Data, Typeable)
 
 main :: IO ()
 main = do
     opts <- cmdArgs program
     let fn  = rafFile opts
-        fn' = fromMaybe (fn ++ ".dat") (datFile opts)
+        fn' = fromMaybe (fn `addExtension` "dat") (datFile opts)
     raf  <- parseFromFile fn fn'
     case raf of
         Left e    -> do
@@ -41,26 +45,48 @@ main = do
               else
                 exitWith (ExitFailure 1)
   where
-    program = Extract { rafFile = def &= argPos 0
-                                      &= typFile
-                      , datFile = def &= name "data"
-                                      &= explicit
-                                      &= typFile
-                                      &= help ("Explicitly specify the"
-                                           ++ " .RAF.DAT file; defaults to "
-                                           ++ "FILE.dat.")
-                      , targets = def &= args
-                                      &= typ "TARGETS"
+    program = Extract { rafFile   = def
+                                  &= argPos 0
+                                  &= typFile
+                      , datFile   = def
+                                  &= name "data"
+                                  &= explicit
+                                  &= typFile
+                                  &= help ("Explicitly specify the "
+                                        ++ ".RAF.DAT file; defaults to "
+                                        ++ "FILE.dat.")
+                      , targets   = def
+                                  &= args
+                                  &= typ "TARGETS"
+                      , baseName  = False
+                                  &= name "b"
+                                  &= name "base"
+                                  &= explicit
+                                  &= help ("Extract file to basename instead "
+                                        ++ "of full file path.")
+                      , changeDir = def
+                                  &= name "C"
+                                  &= name "directory"
+                                  &= explicit
+                                  &= help ("Change into DIR before "
+                                        ++ "extraction.")
                       }
 
 extract :: Extract -> RAF -> FilePath -> IO Bool
-extract opts raf fn = maybe bail write (M.lookup fn raf)
+extract opts raf trg = do
+    createDirectoryIfMissing True cwd
+
+    case M.lookup trg raf of
+        Nothing -> do
+            hPutStrLn stderr $ trg ++ " not found"
+            return False
+        Just bs -> do
+            let (fp, fn) = splitFileName trg
+                ofn | baseName opts = cwd </> fn
+                    | otherwise     = cwd </> fp ++ fn
+            unless (baseName opts) $ do
+                createDirectoryIfMissing True $ cwd ++ fp
+            B.writeFile ofn (decompress bs)
+            return True
   where
-    bail = do
-        hPutStrLn stderr $ fn ++ " not found"
-        return False
-
-    write bs = do
-        B.writeFile (takeFileName fn) (decompress bs)
-        return True
-
+    cwd = fromMaybe "." $ changeDir opts
